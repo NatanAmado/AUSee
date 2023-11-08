6603fc4fca53cb9bee530c764f628e50e06e2bef
ovirandunu <133049040+ovirandunu@users.noreply.github.com>
2023-11-08 16:33:23 +0100

    resolve models

--- CourseReviews1/reviews/forms.py
+++ CourseReviews1/reviews/forms.py
@@ -6,10 +6,10 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ['rating', 'text', 'teacher_name', 'teacher_quality']
         widgets = {
-            'rating': forms.NumberInput(attrs={'placeholder': 'Enter rating (1.0-5.0)'}),
+            
             'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Share your thoughts'}),
             'teacher_name': forms.TextInput(attrs={'placeholder': 'Teacher\'s name (optional)'}),
-            'teacher_quality': forms.NumberInput(attrs={'placeholder': 'Teacher quality (1-5)'}),
+            
         }
         help_texts = {
             'rating': 'Enter a rating between 1.0 and 5.0',
