68814f451c34faa4be7a89922e912d4b9c8c32a6
ovirandunu <133049040+ovirandunu@users.noreply.github.com>
2023-11-09 22:13:03 +0100

    .

--- CourseReviews1/reviews/forms.py
+++ CourseReviews1/reviews/forms.py
@@ -7,13 +7,21 @@ class ReviewForm(forms.ModelForm):
         fields = ['rating', 'text', 'teacher_name', 'teacher_quality']
         widgets = {
             
-            'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Share your thoughts'}),
-            'teacher_name': forms.TextInput(attrs={'placeholder': 'Teacher\'s name (optional)'}),
+            'text': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Share your thoughts about this course... '}),
+            'teacher_name': forms.TextInput(attrs={'placeholder': ''}),
             
         }
         help_texts = {
             'rating': 'Enter a rating between 1.0 and 5.0',
-            'text': 'Share your detailed feedback about the course',
-            'teacher_name': '(Optional) Enter the teacher\'s name',
-            'teacher_quality': '(Optional) Rate the teacher\'s quality on a scale of 1 to 5',
+            'teacher_name': '(Optional)',
+            'teacher_quality': '(Optional)',
         }
+        labels = {
+            'text': 'Review:',
+            'teacher_name': 'Teacher Name:',
+            'teacher_quality': 'Teacher Quality:',
+        }
+
+        
+
+
