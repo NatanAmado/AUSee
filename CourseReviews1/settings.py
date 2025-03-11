66c0d869a6e47ab3619761e1779e65e087e93cd0
ovi <ovirandunu@gmail.com>
2025-03-11 11:44:29 +0000

    Update database configuration to use environment variables

--- CourseReviews1/settings.py
+++ CourseReviews1/settings.py
@@ -103,11 +103,17 @@ WSGI_APPLICATION = 'CourseReviews1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'defaultdb'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 
 
 
