1675f55d18d6e0a413f7d7ff329ae1cadf8ab48a
ovi <ovirandunu@gmail.com>
2025-03-11 11:50:08 +0000

    Add python-dotenv for environment variable management

--- CourseReviews1/settings.py
+++ CourseReviews1/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 from pathlib import Path
-
+# Load environment variables from .env file
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
